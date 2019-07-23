d174fc3fee4253dc614066f1b9236642411cf637
James Cox <jamesncox@yahoo.com>
2019-07-23 16:27:37 +0000

    Done.

--- spec/factories/names.rb
+++
@@ -1,9 +0,0 @@
-FactoryBot.define do
-  factory :name do
-    coupons { "MyString" }
-    [coupon_code, { "MyString" }
-    store] { "MyString" }
-    [string, { "MyString" }
-    string] { "MyString" }
-  end
-end
