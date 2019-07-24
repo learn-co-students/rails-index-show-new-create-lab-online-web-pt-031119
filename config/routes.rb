6113a2d916a15b5a0b0d3e9f4b17863f9379caed
“Mark <mkanko66@gmail.com>
2019-07-23 18:35:17 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+  resources :coupons, only: [:index, :new, :show, :create]
+
+end 
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +59,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
